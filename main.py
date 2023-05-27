8153207583a4b0f7ced4a789351633ea9fdaaf3d
Atychis <ricjimenez009@gmail.com>
2023-05-27 13:00:53 -0500

    update main

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ import os
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def index():
-    return render_template("index.html")
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True, port=os.getenv("PORT", default=8000)
